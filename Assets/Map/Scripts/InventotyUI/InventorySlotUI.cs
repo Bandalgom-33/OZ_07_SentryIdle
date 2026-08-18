5675fb5e176adef9e1f099347c87cade32bdedde
jh9683kim-commits <jh9683kim@gmail.com>
2026-08-18 15:59:03 +0900

    Feat : 인벤토리UI 와 아이템 소스 구현

---
+++ Assets/Map/Scripts/InventotyUI/InventorySlotUI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventorySlotUI : MonoBehaviour
+{
+    [SerializeField] private Image itemIcon;
+    [SerializeField] private TMP_Text quantityText;
+
+    public void SetSlot(InventorySlotData slotData)
+    {
+        if(slotData == null)
+        {
+            itemIcon.enabled = false;
+            quantityText.text = "";
+            return;
+
+            itemIcon.enabled = true;
+            itemIcon.sprite = slotData.itemData.ItemIcon;
+            quantityText.text = slotData.quantity.ToString();
+        }
+    }
+    
+
+}
---
+++ Assets/Map/Scripts/InventotyUI/InventoryView.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class InventoryView : MonoBehaviour
+{
+    //실제 인벤토리 데이터를 가지고 있는 객체
+    [SerializeField] private InventoryGridManager inventoryGridManager;
+    //만든 ItemSlot Prefab
+    [SerializeField] private InventorySlotUI itemSlotPrefab;
+    //슬롯들이 생성될 부모
+    [SerializeField] private Transform itemSlotRoot;
+    
+    private void Start()
+    {
+        CreateSlots();
+    }
+
+    private void CreateSlots()
+    { 
+        if (inventoryGridManager == null || itemSlotPrefab == null || itemSlotRoot == null) return;
+        
+        for (int i = 0; i < inventoryGridManager.Slots.Count; i++)
+        {
+            InventorySlotUI slotUI = Instantiate(itemSlotPrefab, itemSlotRoot);
+            slotUI.SetSlot(inventoryGridManager.Slots[i]);
+        }
+    }
+}
